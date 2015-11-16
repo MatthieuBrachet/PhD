6423b76adf5dc5c974e67c8637be5357a6a086b0
MatthieuBrachet <brachetmat@free.fr>
2015-11-16 09:54:17 +0100

    update test on spherical harmonic

--- test harmonique spherique/derlegendre.m
+++ test harmonique spherique/derlegendre.m
@@ -7,30 +7,24 @@ function [dpmm] = derlegendre(m,l,z)
 %
 % inspired by W. H. Press and al. in 1992
 %
-% ********************************************
-pmm=1;
-if (m>=0)
-    fact=1;
-    for i=1:m
-        fact=fact+2;
-    end
-    dpmm=m.*fact.*z.*(1-z.^2).^(m/2-1);
-    if (l == m)
-        dpmm=dpmm;
+% ********************************************;
+
+fact=prod(1:2:2*m-1);
+dpmm=0;
+if (l == m)
+    dpmm=-m*fact*z.*(1-z.^2)^(m/2-1);
+else
+    dpmmp1=-m*fact*z.*(1-z.^2)^(m/2-1);
+    if (l == m+1)
+        pmmp1= legendre(m,l,z);
+        dpmm=(2*m-1).*pmmp1+z.*(2*m+1).*dpmmp1;
     else
-        pmm=legendre(m,m,z);
-        pmmp1=dpmm;
-        if (l == m+1)
-            dpmm=(2*m+1)*pmm+z.*(2*m+1).*dpmm;
-        else
-            for ll=m+2:l
-                pmm=legendre(m,ll,z);
-                pll=((2*ll-1)*pmm+z.*(2*l-1).*dpmm-(ll+m-1)*pmmp1)/(ll-m);
-                dpmm=pmmp1;
-                pmmp1=pll;
-            end
-            dpmm=pll;
+        for ll=m+2:l
+            p=legendre(m,ll-1,z);
+            dpmm=(2*ll-1).*p+z.*(2*ll-1).*dpmm-(ll+m-1).*dpmmp1;
+            dpmm=dpmm./(l-m);
         end
     end
 end
+
 end
