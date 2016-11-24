49c4eab61449fd1a7d6f6ae326c536caede586f5
MatthieuBrachet <brachetmat@free.fr>
2016-11-24 08:46:56 +0100

    add earth topology and new results

--- test SWEC Williamson/topography.m
+++ test SWEC Williamson/topography.m
@@ -6,19 +6,6 @@ xx=pi/180*[1:1:720]-2*pi;
 yy=pi/180*[1:180]-pi/2;
 [X,Y]=meshgrid(xx,yy);
 altp=interp2(X,Y,rel,lambda,teta);
-
 sud=mean(topo(1,:));
-nord=mean(topo(end,:));
-
-[n1,n2]=size(x);
-for i=1:n1
-    for j=1:n2
-        if abs(z(i,j)-rr)<10^-3
-            alt(i,j)=nord;
-        elseif abs(z(i,j)+rr)<10^-3
-            alt(i,j)=sud;
-        else
-            alt(i,j)=altp(i,j);
-        end
-    end
-end
+altp(isnan(altp))=0;
+alt=altp.*(1-(z==-rr))+sud.*(z==-rr);
