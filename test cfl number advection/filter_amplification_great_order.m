0e61fd06a0889890c7f3f6b7c12b37018a27c8ac
Matthieu Brachet <brachet1@fst-iec-the-l07.iecl-ncy.site.univ-lorraine.fr>
2017-10-19 19:31:40 +0200

    redaction

--- test cfl number advection/filter_amplification_great_order.m
+++ test cfl number advection/filter_amplification_great_order.m
@@ -2,24 +2,35 @@ clc; clear all; close all;
 
 teta=linspace(0,pi,10000);
 
-J1=5;
-[ ampli1 ] = ampli_ftr( J1, teta );
-max(ampli1)
-J2=10;
-[ ampli2 ] = ampli_ftr( J2, teta );
-max(ampli2)
-J3=15;
-[ ampli3 ] = ampli_ftr( J3, teta );
-max(ampli3)
-J4=20;
-[ ampli4 ] = ampli_ftr( J4, teta );
-max(ampli4)
-J5=30;
-[ ampli5 ] = ampli_ftr( J5, teta );
-max(ampli5)
+J1=1;
+ampli1=1-(1/(-2)^J1).*(cos(teta)-1).^J1;
 
+J2=2;
+ampli2=1-(1/(-2)^J2).*(cos(teta)-1).^J2;
+
+J3=3;
+ampli3=1-(1/(-2)^J3).*(cos(teta)-1).^J3;
+
+J4=4;
+ampli4=1-(1/(-2)^J4).*(cos(teta)-1).^J4;
+
+J5=5;
+ampli5=1-(1/(-2)^J5).*(cos(teta)-1).^J5;
 
 figure(1)
-plot(teta,ampli1,teta,ampli2,teta,ampli3,teta,ampli4,teta,ampli5,'Linewidth',2)
+plot(teta,ampli1,teta,ampli2,teta,ampli3,teta,ampli4,teta,ampli5,'Linewidth',1.5)
 legend(['order ' num2str(2*J1)],['order ' num2str(2*J2)],['order ' num2str(2*J3)],['order ' num2str(2*J4)],['order ' num2str(2*J5)],'Location','southwest');
-title('Filter amplification')
+%title('Filter amplification')
+grid on
+xlabel('\theta')
+ylabel('\beta(\theta)')
+
+for J=1:5
+    teta_m=acos(1-2*(.05)^(1/J))
+end
+
+J=linspace(1,100,50);
+teta_m=acos(1-2.*(.05).^(1./J));
+
+figure(2)
+plot(J,teta_m)
