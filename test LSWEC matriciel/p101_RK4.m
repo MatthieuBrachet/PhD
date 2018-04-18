c7c859b75dceda0156c08a2633c892a3c91f13f4
Matthieu Brachet <brachet1@fst-iec-the-l07.iecl-ncy.site.univ-lorraine.fr>
2018-04-18 16:43:23 +0200

    update redaction

--- test LSWEC matriciel/p101_RK4.m
+++ test LSWEC matriciel/p101_RK4.m
@@ -23,13 +23,13 @@ global opt_ftr test scheme detec
 global hp gp u0 radius omega
 global teta0 teta1
 
-test=1;
+test=0;
 video = 'no';
-sauvegarde = 0;
+sauvegarde = 1;
 opt_ftr='redonnet10';
 type_ftr='symetric';
 scheme='compact4';
-n=31;
+n=255;
 mod101
 
 teta0=-pi/3;
@@ -42,7 +42,7 @@ c=max(cgrav,ccor);
 cfl=0.9;
 ddt=radius*dxi*cfl/c;
 ndaymax=5;
-Tmax=1.5*3600;%ndaymax*3600*24;
+Tmax=ndaymax*3600*24;
 itermax=100000;
 
 %% *** initialisation des données
